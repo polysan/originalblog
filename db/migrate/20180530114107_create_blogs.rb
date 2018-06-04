4cc3c2fb31319e1294fd085e18d6f9e48883f2a7
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2018-06-04 20:19:25 +0900

    Revert "User.controller作成"
    This reverts commit c97139e5c4ee03fe02840a526cbde4513ed926e9.

--- db/migrate/20180530114107_create_blogs.rb
+++ db/migrate/20180530114107_create_blogs.rb
@@ -1,10 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs do |t|
-      t.string :title
-      t.text :text
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
