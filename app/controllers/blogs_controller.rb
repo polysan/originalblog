c39f24729ab1ade17b6658ffafa2adaac30ed017
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2018-06-09 15:46:13 +0900

    Createメソッドとストロングパラメーターを記述

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,14 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
+  def create
+    Blog.create(blog_params)
+  end
 
+private
+  def blog_params
+    params.require(:blog).permit(:title,:text)
+  end
 
 
 end
