0599e98d4f6840d964fef86cb1a26a08202a79bd
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2018-06-09 16:19:13 +0900

    blogs.controllerにpage(params[;page]).per(5)を記入

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   def index
-    @blogs = Blog.order("created_at DESC")
+    @blogs = Blog.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
