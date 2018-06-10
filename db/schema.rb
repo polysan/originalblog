8af047296d2619a2c7188a306bdaa4a97b603e1d
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2018-06-10 17:51:45 +0900

    ターミナルで rails g migration AddUserIdToTweets user_id:integer を実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180610070735) do
+ActiveRecord::Schema.define(version: 20180610085048) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "text",       limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
