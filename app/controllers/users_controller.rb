a96276ed062dc02549b337256f55eb82a767b38a
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2018-06-11 18:58:51 +0900

    Userscontroller showアクションにMaterializeのデザインを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
 @nickname = current_user.nickname
-@blog = Blogs.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+@blogs = Blog.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
   end
 end
